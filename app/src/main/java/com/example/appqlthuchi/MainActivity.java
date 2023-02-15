9cef1f769e4c1e155820834c2b7ab647b720df7b
AnhTran <letrannhatanh171@gmail.com>
2023-02-15 09:46:45 +0700

    test

--- app/src/main/java/com/example/appqlthuchi/MainActivity.java
+++ app/src/main/java/com/example/appqlthuchi/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //a.Hello world
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
